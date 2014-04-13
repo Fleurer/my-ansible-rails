dcf86b56713c3e230361f52c7d19559929f8b795
Li Yazhou <me.ssword@gmail.com>
2014-04-13 18:46:52 +0800

    lock up redis-version

--- provision.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-ansible-playbook provisioning/playbook.yml -i provisioning/ansible_hosts -u vagrant --private-key=~/.ssh/id_rsa -vvv --extra-vars "app_user=vagrant"
-# ansible-playbook provisioning/playbook.yml -i provisioning/ansible_hosts -u coinbaz --private-key=~/.ssh/id_rsa -vvv --extra-vars "app_user=coinbaz"
