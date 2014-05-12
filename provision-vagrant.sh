0b99338dd83c44a29eea8061aed83af011da4756
Wu Changlong <clongbupt@gmail.com>
2014-05-12 14:29:53 +0800

    update start shell

--- provision-vagrant.sh
+++ provision-vagrant.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ansible-playbook provisioning/playbook.yml -i provisioning/ansible_hosts -u vagrant --private-key=~/.ssh/id_rsa -vvv --extra-vars "app_user=vagrant"
-# ansible-playbook provisioning/playbook.yml -i provisioning/ansible_hosts -u coinbaz --private-key=~/.ssh/id_rsa -vvv --extra-vars "app_user=coinbaz"
+#ansible-playbook provisioning/playbook.yml -i provisioning/ansible_hosts -u vagrant --private-key=~/.ssh/id_rsa -vvv --extra-vars "app_user=vagrant"
+ansible-playbook provisioning/playbook.yml -i provisioning/ansible_hosts -u coinbaz --private-key=~/.ssh/id_rsa -vvv --extra-vars "app_user=coinbaz"
